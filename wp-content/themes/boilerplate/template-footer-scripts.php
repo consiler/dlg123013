d2f9cd516e4253fb0ec73ff4744a09a70fb72976
MP <consiler@gmail.com>
2013-12-30 17:52:09 -0800

    general cleaning

---
+++ wp-content/themes/boilerplate/template-footer-scripts.php
@@ -0,0 +1,20 @@
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/foundation/js/foundation/foundation.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/foundation/js/foundation/foundation.orbit.js"></script>
+<?php if(is_front_page()) { ?>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/slider.js"></script>
+<?php } ?>
+<script>
+  //GA
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-44776256-1']);
+  _gaq.push(['_setDomainName', 'dialoggroup.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  </script>
+<script type="text/javascript">
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
