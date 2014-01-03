cd8386a883a0cb0c530be097d198c18ff3951500
MP <consiler@gmail.com>
2014-01-02 17:40:53 -0800

    starting using get_template_part instead of include for page templates

--- wp-content/themes/boilerplate/page.php
+++ wp-content/themes/boilerplate/page.php
@@ -4,17 +4,17 @@
  */
 get_header(); ?>
 <div id="internal-wrap">
-  <?php include 'internal_header.php'; ?>
+  <?php get_template_part('internal_header'); ?>
   <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
   <div id="internal-lower-wrap">
     <div id="internal-lower" class="centered">
       <div id="internal-main"<?php if(!get_field('has_sidebar')){ ?> class="internal-main-fullwidth"<?php } ?>>
         <?php
-          include 'template-left-sidebar.php';
-          include 'template-main-content.php';
+          get_template_part('template', 'left-sidebar');
+          get_template_part('template', 'main-content');
         ?>
       </div>
-      <?php include 'template-right-sidebar.php'; ?>
+      <?php get_template_part('template', 'right-sidebar'); ?>
     </div>
   </div>
   <?php endwhile; ?>
