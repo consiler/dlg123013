41461f09ede4d37bea545acd19656adf6ab2f4b7
MP <consiler@gmail.com>
2014-01-05 23:29:40 -0800

    doing k12

--- wp-content/themes/boilerplate/template-k12.php
+++ wp-content/themes/boilerplate/template-k12.php
@@ -4,11 +4,15 @@
  */
 get_header(); ?>
 <div id="internal-wrap">
+  <?php get_template_part('template', 'internal-header'); ?>
   <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
   <div id="internal-lower-wrap">
     <div id="internal-lower" class="centered">
-      <div id="k12-rightalign">
-        
+      <div id="k12-wrap">
+        <div id="k12-intro">
+          <h2>Classrooms and Opportunities Transformed</h2>
+          <p>;ladjsfniaprngkl;asd</p>
+        </div>
       </div>
     </div>
   </div>
