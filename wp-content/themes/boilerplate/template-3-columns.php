ef7399fe9c98d3470eff9ed0a47430665f40340f
MP <consiler@gmail.com>
2014-01-14 03:57:16 -0500

    3 column

--- wp-content/themes/boilerplate/template-3-columns.php
+++ wp-content/themes/boilerplate/template-3-columns.php
@@ -1,10 +1,10 @@
       <?php if (get_field('include_three_column')) { ?>
-      <div id="three-column" class="grid-env">
+      <div id="three-column" class="block-grid-env">
         <?php if(get_field('three_column_data')): ?>
           <?php while(has_sub_field('three_column_data')): ?>
-          <div class="row">
+          <ul class="small-block-grid-1 medium-block-grid-3">
             <?php for($i = 1; $i <= 3; $i++) { ?>
-              <div class="large-4 columns threecolumn-cell">
+              <li class="threecolumn-cell">
               <?php if($header = get_sub_field('header_'.$i)) { ?>
                 <h4<?php $cs = get_page_template_slug(get_the_ID()); if($cs == "template-case-study-page.php") { echo ' class="cs"'; } ?>><?php echo $header; ?></h4>
                 <?php if($copy = get_sub_field('copy_'.$i)) { ?>
@@ -13,9 +13,9 @@
                 <?php if(($cta = get_sub_field('cta_'.$i)) && ($cta_link = get_sub_field('cta_link_'.$i))) { ?>
                 <a href="<?php echo $cta_link; ?>"><span class="light-grey-button"><?php echo $cta; ?></span></a>
                 <?php } } ?>
-            </div>
+            </li>
             <?php } ?>
-          </div>
+          </ul>
           <?php endwhile; ?>
         <?php endif; ?>
       </div>
