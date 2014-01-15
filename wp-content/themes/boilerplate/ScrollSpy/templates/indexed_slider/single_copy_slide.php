c36ba391a904ac51fec596693eac7cb6a145478e
sanford78 <sanford78@gmail.com>
2014-01-15 17:56:33 -0500

    bg to copy slides

--- wp-content/themes/boilerplate/ScrollSpy/templates/indexed_slider/single_copy_slide.php
+++ wp-content/themes/boilerplate/ScrollSpy/templates/indexed_slider/single_copy_slide.php
@@ -1,6 +1,6 @@
 <?php $copy = $slide["copy"]; ?>
 <div class="promo-slide">
-  <div class="promo-slide-text">
-    <?php echo $copy; ?>
+  <div class="promo-slide-text" style= "<?php echo $slide["background"]; ?>">
+    <?php echo $copy;?>
   </div>
 </div>
