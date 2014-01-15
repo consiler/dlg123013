4c1734e03aa68e267e2ef08622c30c2b05cb4a96
Will Hoback <will.hoback@gmail.com>
2014-01-15 17:13:57 -0600

    careers page

--- wp-content/themes/boilerplate/template-career-page.php
+++ wp-content/themes/boilerplate/template-career-page.php
@@ -14,51 +14,88 @@ get_header(); ?>
           <div id="career-learn-more-wrap">
             <div id="career-learn-more">
               <a href="\our-team"><span class="career-learn-more-button">Learn more about our team</span></a>
-            </div>
-          </div>
+            </div><!-- career-learn-more -->
+          </div><!-- career-learn-more-wrap -->
           <div id="value">
             <h2><?php the_field('center_text');?></h2>
             <h3><?php the_field('sub_center_text');?></h3>
+          </div><!-- value -->
+            <div id="three-column" class="block-grid-env">
+              <ul class="small-block-grid-1 medium-block-grid-3">
+                <li class="threecolumn-cell">
+                  <img src="<?php bloginfo('template_url')?>/images/career/offer1.png">
+                  <h2 class="values">Excellent Work</h2>
+                  <p>The opportunity to work on cutting edge problems for leading companies.</p>
+                </li>
+                <li class="threecolumn-cell">
+                  <img src="<?php bloginfo('template_url')?>/images/career/offer2.png">
+                <h2 class="values">Lifestyle Design</h2>
+                <p>The opportunity to develop customizable, integral development plan that.</p>
+                </li>
+                <li class="threecolumn-cell">
+                  <img src="<?php bloginfo('template_url')?>/images/career/offer3.png">
+                <h2 class="values">Purpose</h2>
+                <p>Helping to write at least a portion of the book on 21st century management and helping to create the abundant world of the future.</p>
+                </li>
+              </ul>
+          </div><!-- three-column -->
+          
+         <div id="job-listings-wrap">
+          <div id="open-jobs-image">
+            <img id="open-jobs"src="<?php bloginfo('template_url')?>/images/career/current-jobs.png">
           </div>
-          <?php get_template_part('template', '3-columns'); ?>
-
-          <div id="job-listings-wrap">
-            <div id="open-jobs-image">
-              <img id="open-jobs"src="<?php the_field('open_jobs_image')?>">
-            </div>
-            <div id="job-listings">
-              <h2 id="position">Position</h2>
-              <h2 id="location">Location</h2>
+          <div id="job-listings">
+           <h2 id="position">Position</h2>
+           <h2 id="location">Location</h2>
                <section id="jobs">            
                 <? $args = array( 'post_type' => 'job_listings' );
                    $loop = new WP_Query( $args );
                    while ( $loop->have_posts() ) : $loop->the_post();?>
-                <header class="jobs-header">
-                  <div class="jobs-pos"><?php the_field('position');?></div>
-                  <div class="jobs-loc"><?php the_field('location');?></div>
-                  <div class="jobs-item-image">
-                  <a href="#">
-                          <img src="<?php bloginfo('url'); ?>/wp-content/uploads/2013/12/job-detail-button_16.png"/>
-                        </a>
-                      </div>
-                      
-                    </header>
+                  <header class="jobs-header">
+                    <div class="jobs-pos"><?php the_field('position');?></div>
+                    <div class="jobs-loc"><?php the_field('location');?></div>
+                    <div class="jobs-item-image">
+                      <a href="#">
+                        <img src="<?php bloginfo('url'); ?>/wp-content/uploads/2013/12/job-detail-button_16.png"/>
+                      </a>
+                    </div>
+                  </header>
+                    <hr class="embosed">
                    <?php endwhile; ?>
-                  
-
               </section>
-             
-            </div> 
+            </div> <!-- job-listings -->
             <div id="jobs-contact">
-              <p>Please send all inquires to:</p>
-              <a href="mailto:jobs@dialoggroup.com">Jobs@DialogGroup.com</a>
-              </div>
-          </div>
+              <p>Please send all inquires to:<a href="mailto:jobs@dialoggroup.com">Jobs@DialogGroup.com</a></p>
+            </div>
+          </div><!-- job-listings-wrap -->
 
+          <div id="career-testimonial" class="centered">
+                  <div class="slider">
+                    <ul class="slides">
+                      <li class="slide testimonial"></li>
+                      
+                    </ul>
 
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+                  </div>
+                </div>
+            </div>
+          
+        </div><!-- career-page -->
+      </div><!--internal-main-->
+    </div><!-- internal-lower -->
+  </div><!-- internal-lower-wrap -->
+</div><!-- internal-wrap -->
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/glide.js"></script>
+
+<script type="text/javascript">
+
+    $('.slider').glide({
+      autoplay: 'false',
+      arrows: '.slider',
+      nav: 'false',
+      arrowRightText: '',
+      arrowLeftText: ''
+    });
+
+</script>
 <?php get_footer(); ?>
